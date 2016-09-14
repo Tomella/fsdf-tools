caf522b55fe8ed2159d0e7e659eb9c141ac1fa53
Larry Hill <tomella1@gmail.com>
2016-09-14 16:50:58 +1000

    Create output directory if it doesn't exist

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var fs = require('fs');
+
+// Make the output directory if it doesn't exist
+try {
+  fs.mkdirSync("output");
+} catch(e) {
+  if ( e.code !== 'EEXIST' ) throw e;
+}
+
 require('./tools/basins.js');
 require('./tools/catchments.js');
